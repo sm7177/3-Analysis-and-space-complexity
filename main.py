8cc26dab8454cd7c6fb5c8e52049d6211e7bf242
sm7177 <subrinamaharjan2020@gmail.com>
2024-10-27 15:43:41 +0545

    .

---
+++ main.py
@@ -0,0 +1,14 @@
+# def sum(n):
+#     return n*(n+1)/2
+
+# print(sum(30))\
+    
+def array(n):
+    sum=0
+    for i in a:
+        sum=sum+i
+        print(i)
+    return(sum)
+
+a=[10,20,27,18,19,97,30]
+print(array(a))
